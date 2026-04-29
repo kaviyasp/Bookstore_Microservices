be1547e17e89892c8bc91baa3a1ecb84f058ea54
Kaviya <kaviyasugumar8@gmail.com>
2026-04-29 11:19:48 +0530

    [Kaviya] User service only

---
+++ user-service/src/main/java/com/bookstore/user/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.bookstore.user;
+
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("User Service API").version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components().addSecuritySchemes("Bearer",
+                        new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
