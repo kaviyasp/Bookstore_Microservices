eba146db48a3fde903b4fe211accbccf0036265b
Kaviya <kaviyasugumar8@gmail.com>
2026-04-29 11:15:23 +0530

    [Kaviya] Config server only

--- config-server/src/test/java/com/bookstore/config/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/bookstore/config/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bookstore.cart;
+package com.bookstore.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CartServiceApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
