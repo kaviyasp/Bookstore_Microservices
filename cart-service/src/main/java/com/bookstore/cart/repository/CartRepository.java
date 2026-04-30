4f49307f4b929e161bf61becfd0267330cce2935
Kaviya <kaviyasugumar8@gmail.com>
2026-04-30 12:04:15 +0530

    [Kaviya] Setup Eureka Server as standalone service

--- cart-service/src/main/java/com/bookstore/cart/repository/CartRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bookstore.cart.repository;
-
-import com.bookstore.cart.model.Cart;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CartRepository extends CrudRepository<Cart, String> {}
